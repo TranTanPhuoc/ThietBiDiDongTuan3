ebc039163d64fdbcc4ff13c0cada0f6202c4f824
Tran Tan Phuoc <trantanphuoc262@gmail.com>
2022-03-06 23:25:05 +0700

    git commit-m "Push layout sc01"

---
+++ app/src/main/java/com/example/tuan3/MainActivity_sc01.java
@@ -0,0 +1,14 @@
+package com.example.tuan3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_sc01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_sc01);
+    }
+}
